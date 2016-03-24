80a6a31d666a0467d893a0bdf3fddecb665e0804
gibbloggen <gibbloggen@gmail.com>
2016-03-23 21:52:04 -0400

    Shopping Mode First Draft done, this might be all that there is for this part of beta 3,,,

---
+++ EssentialGrocer/ShoppingMode.xaml.cs
@@ -0,0 +1,110 @@
+﻿using EssentialGrocer.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace EssentialGrocer
+{
+
+
+    
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShoppingMode : Page
+    {
+        private ObservableCollection<Grocery> GettingGroceries;
+        private ObservableCollection<Grocery> GottenGroceries;
+
+        public ShoppingMode()
+        {
+            this.InitializeComponent();
+
+            GettingGroceries = new ObservableCollection<Grocery>();
+            GottenGroceries = new ObservableCollection<Grocery>();
+            GroceryManager.AsynchInitializingGroceryToGet(GettingGroceries);
+           
+
+        }
+        
+
+
+
+
+
+
+        private void GottenGroceries_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Grocery NotGotten = (Grocery)e.ClickedItem;
+            foreach (Grocery product in GottenGroceries)
+            {
+                if (product.UPC_Code == NotGotten.UPC_Code)
+                {
+                    GottenGroceries.Remove(product);
+                    break;
+                }
+            }
+            GettingGroceries.Add(NotGotten);
+            GettingGroceries.Sort(p => p.Description);
+        }
+
+        private void GettingGroceries_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Grocery Gotten = (Grocery)e.ClickedItem;
+            foreach (Grocery product in GettingGroceries)
+            {
+                if (product.UPC_Code == Gotten.UPC_Code)
+                {
+                    GettingGroceries.Remove(product);
+                    break;
+                }
+            }
+           GottenGroceries.Add(Gotten);
+            GottenGroceries.Sort(p => p.Description);
+
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (GroceryManager.CheckWindowSize(Window.Current))
+            {
+                MySplitView.IsPaneOpen = true;
+            }
+            else
+            {
+                MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+
+            }
+        }
+
+        private void CategoryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (ListCreation.IsSelected)
+            {
+                Frame BackToCreation = (Frame)Parent;
+                BackToCreation.Content = new WorkHorse();
+                
+
+            }
+        }
+    }
+}
